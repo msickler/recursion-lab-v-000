c0ef9b481cc398f54616860b6ef9748254d84d97
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-07 15:01:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 function printString(myString) {
-  console.log(myString[0]);
+  console.log(myString[0])
 
   if (myString.length > 1) {
-    let mySubString = myString.substring(1, myString.length);
-    printString(mySubString);
+    let mySubString = myString.substring(1, myString.length)
+    printString(mySubString)
   } else {
-    return true;
+    return true
   }
 }
 
 function reverseString(myString) {
    if (myString.length > 1) {
-     let mySubString = myString.substring(1, myString.length);
-     return reverseString(mySubString) + myString[0];
+     let mySubString = myString.substring(1, myString.length)
+     return reverseString(mySubString) + myString[0]
    } else {
-     return myString;
+     return myString
    }
  }
 
 function isPalindrome(myString) {
-  let n = myString.length;
+  let n = myString.length
   if (n < 2) {
-   return true;
+   return true
   } else if (myString[n - 1] === myString[0]) {
-   return isPalindrome(myString.substring(1, n - 1));
+   return isPalindrome(myString.substring(1, n - 1))
   } else {
-   return false;
+   return false
   }
 }
 
-function addUpTo(myArray, index) {
+function addUpTo(array, index) {
    if(index > 0){
-     return addUpTo(myArray, index-1) + myArray[index]
+     return addUpTo(array, index-1) + array[index]
    } else {
-     return myArray[index]
+     return array[index]
    }
  }
 
@@ -48,4 +48,11 @@ function maxOf(array) {
   } else {
   	return array[0]
   }
+}
+
+function includesNumber(array, n) {
+  if (array.length === 1) {
+    return (array[0] === n) ? true : false
+  }
+  return (array[0] === n) ? true : includesNumber(array.slice(1), n)
 }
