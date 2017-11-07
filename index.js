09caab3c8244214a67a0c7c922790da680d45f80
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-07 14:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,10 @@ function printString(myString) {
 }
 
 function reverseString(myString) {
-  console.log(myString[0]);
-
-  if (myString.length > 1) {
-    let mySubString = myString.substring(1, myString.length);
-    reverseString(mySubString) + myString[0];
-  } else {
-    return myString;
-  }
-}
+   if (myString.length > 1) {
+     let mySubString = myString.substring(1, myString.length);
+     return reverseString(mySubString) + myString[0];
+   } else {
+     return myString;
+   }
+ }
